ea0186525508f6ce3bf98dfbf204d5970d347c91
Ume123 <45653197+Ume123@users.noreply.github.com>
2019-11-12 20:25:41 +0300

    Add files via upload

--- cinemaparser/core.py
+++ cinemaparser/core.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 class CinemaParser:
     """ Класс нужен для оперирования с сайтом https://msk.subscity.ru/ """
-    def __init__(self, name):
+    def __init__(self, name=" "):
         """Функция используется для получения html страницы сайта"""
         self.city = name
         if self.city == "Москва":
@@ -49,46 +49,59 @@ class CinemaParser:
         return link
 
     def get_first_session(self, film_name):
-        """Функция ищет ссылку на фильм по его названию и выводит ближайщий"""
+        """Функция ищет ссылку на фильм по его названию и выводит ближайщий сеанс и кинотеатр"""
         if not self.content:
             self.extract_raw_content()
         for data in self.content.find_all('div', class_="movie-plate"):
             if data['attr-title'] == film_name:
                 next_session_time = data['attr-next-screening']
-                next_session_time_translation = time.ctime(int(i['attr-next-screening']))
-                div_with_href = (i.find('div', class_='text-center movie-poster-mobile'))
+                next_session_time_translation = time.ctime(int(data['attr-next-screening']))
+                div_with_href = (data.find('div', class_='text-center movie-poster-mobile'))
                 a_with_href = div_with_href.find('a')['href']
                 link = self.url + a_with_href
 
         try:
-            a = link
+            per = link
         except  UnboundLocalError:
             return (None, None)
 
         new_page = requests.get(link)
         page_film = BeautifulSoup(new_page.text, "html.parser")
-
         for first_table in page_film.find_all('tr', class_="row-entity"):
             table_with_time = first_table.find('td', class_='text-center cell-screenings')
-            if table_with_time['attr-time'] == next_session_time:
-                name_film = first_table.find('a', class_="underdashed").get_text()
+            if int(table_with_time['attr-time']) == int(next_session_time):
+                name_cinema = first_table.find('a', class_="underdashed").get_text()
 
-        return name_film, next_session_time_translation
+        return name_cinema, next_session_time_translation
 
-    def get_nearest_session(self):
-#        if not self.content:
-#            self.extract_raw_content()
-        page=page.read(page.html)
-        self.content = BeautifulSoup(page, "html.parser")
-        dict_times={}
-        dict_movie_plates={}
-        movie_plates=self.content.find_all('div', class_="movie-plate")
-        print(movie_plates[0]['attr-next-screening'])
-        for i in range(len(movie_plates)):
-            dict_times.update({int(movie_plates[i]['attr-next-screening']):i})
-            dict_movie_plates.update({movie_plates[i]:i})
-       # print(dict_movie_plates)
+    def get_soonest_session(self):
+        """Функция используется для поиска ближайшего сеанса и кинотеатра"""
+        if not self.content:
+            self.extract_raw_content()
+        dict_movie_plates = {}
+        times = []
+        movie_plates = self.content.find_all('div', class_="movie-plate")
+        for movie_plate in movie_plates:
+            dict_movie_plates.update({int(movie_plate['attr-next-screening']):movie_plate})
 
-#CINEMAPARSER = CinemaParser('Москва')
-#print(CINEMAPARSER.get_films_list())
-#print(CINEMAPARSER.get_nearest_session()
+        storted_times = sorted(dict_movie_plates.keys())
+        soonest_plate = dict_movie_plates.get(storted_times[0])
+
+        div_with_href = (soonest_plate.find('div', class_='text-center movie-poster-mobile'))
+        a_with_href = div_with_href.find('a')['href']
+        link = self.url + a_with_href
+
+        new_page = requests.get(link)
+        page_film = BeautifulSoup(new_page.text, "html.parser")
+        for first_table in page_film.find_all('tr', class_="row-entity"):
+            table_with_time = first_table.find('td', class_='text-center cell-screenings')
+            times.append(table_with_time['attr-time'])
+            if int(table_with_time['attr-time']) == storted_times[0]:
+                name_cinema = first_table.find('a', class_="underdashed").get_text()
+        try:
+            per = name_cinema
+        except  UnboundLocalError:
+            return (None, None, None)
+
+        return (name_cinema, soonest_plate['attr-title'], time.ctime(int(storted_times[0])))
+
