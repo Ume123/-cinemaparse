1f0cad1df085abe02e00a8cabfdf005fd6073861
Ume123 <45653197+Ume123@users.noreply.github.com>
2019-11-12 21:12:33 +0300

    Delete example.py

--- example.py
+++
@@ -1,5 +0,0 @@
-""" Пример использования класса CinemaParser"""
-from cinemaparser import CinemaParser
-CINEMAPARSER = CinemaParser('Москва')
-print(CINEMAPARSER.get_films_list())
-print(CINEMAPARSER.get_first_session('Джокер'))
